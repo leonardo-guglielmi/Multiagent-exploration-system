b56edb429577841d1141305f821dd33e6f6f1bac
guflie <guglie.leonardo@gmail.com>
2025-01-28 14:55:06 +0100

    Add backhaul configuration parameter

--- Control_function_config_DTO.py
+++ Control_function_config_DTO.py
@@ -1,7 +1,8 @@
 class Control_function_DTO:
-    def __init__(self, type_of_search, type_of_coverage, type_of_exploration, expl_weight, is_concurrent):
+    def __init__(self, type_of_search, type_of_coverage, type_of_exploration, expl_weight, is_concurrent, backhaul_network_available):
         self.type_of_search = type_of_search
         self.type_of_coverage = type_of_coverage
         self.type_of_exploration = type_of_exploration
         self.expl_weight = expl_weight
         self.is_concurrent = is_concurrent
+        self.backhaul_network_available = backhaul_network_available
