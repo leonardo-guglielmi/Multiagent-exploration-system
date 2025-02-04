090f2c4b48fc1637e43550297d4cd685d1aa47fe
guflie <guglie.leonardo@gmail.com>
2025-02-04 22:48:21 +0100

    Add third experiment

--- main.py
+++ main.py
@@ -72,6 +72,33 @@ def experiment2():
             f.write(str(e))
         raise e
 
+def experiment3():
+    try:
+        types_of_search = ["local"]
+        expl_weights = ["constant", "decrescent"]
+
+        for i in range(NUM_OF_SIMULATIONS):
+            deserialize = False
+            for type_of_search in types_of_search:
+                for expl_weight in expl_weights:
+                    print(f'----- Starting simulation [{type_of_search}-{expl_weight}] : {i} @ {date.now()}-----')
+                    with open("logs/output_log.txt", 'a') as f:
+                        f.write(f'----- Starting simulation [{type_of_search}-{expl_weight}] : {i} @ {date.now()}-----\n')
+
+                    simulate(type_of_search, expl_weight, i, deserialize, use_expl=True, use_bs=True)
+                    Sensor.id = 0
+                    User.id = 0
+                    deserialize = True
+        print("Simulations completed")
+        with open("logs/output_log.txt", 'a') as f:
+            f.write("Simulations completed\n")
+
+    except Exception as e:
+        with open("logs/error_log.txt", "w") as f:
+            f.write(str(e))
+        with open("logs/output_log.txt", 'a') as f:
+            f.write(str(e))
+        raise e
 
 if __name__ == '__main__':
-    experiment2()
+    experiment3()
