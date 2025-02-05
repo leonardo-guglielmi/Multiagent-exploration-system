c2d04e1af603274f0f8b7d2c432c8185143f1a29
guflie <guglie.leonardo@gmail.com>
2025-02-05 21:58:27 +0100

    fixup! Add custom probability option

--- Control_function_config_DTO.py
+++ Control_function_config_DTO.py
@@ -1,8 +1,9 @@
 class Control_function_DTO:
-    def __init__(self, type_of_search, type_of_exploration, expl_weight, is_concurrent, backhaul_network_available, use_expl):
+    def __init__(self, type_of_search, type_of_exploration, expl_weight, is_concurrent, backhaul_network_available, use_expl, use_custom_prob):
         self.type_of_search = type_of_search
         self.type_of_exploration = type_of_exploration
         self.expl_weight = expl_weight
         self.is_concurrent = is_concurrent
         self.backhaul_network_available = backhaul_network_available
         self.use_expl = use_expl
+        self.use_custom_prob = use_custom_prob
