2500bdc2d90c5c56307b7d3ad334840aa1db3ddd
guflie <guglie.leonardo@gmail.com>
2025-02-09 12:24:10 +0100

    Add comment

--- Control_function_config_DTO.py
+++ Control_function_config_DTO.py
@@ -2,7 +2,7 @@ class Control_function_DTO:
     def __init__(self, type_of_search, type_of_exploration, expl_weight, is_concurrent, backhaul_network_available, use_expl, use_custom_prob):
         self.type_of_search = type_of_search
         self.type_of_exploration = type_of_exploration
-        self.expl_weight = expl_weight
+        self.expl_weight = expl_weight # Now useless
         self.is_concurrent = is_concurrent
         self.backhaul_network_available = backhaul_network_available
         self.use_expl = use_expl
